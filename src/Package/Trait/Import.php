6d1ac530f74836afd4118d2a691f2b6dafc91cf5
remco van der Velde <remco.pc@outlook.com>
2023-12-22 12:56:41 +0100

    system config

--- src/Package/Trait/Import.php
+++ src/Package/Trait/Import.php
@@ -14,8 +14,11 @@ trait Import {
     public function role_system(): void
     {
         $object = $this->object();
-        $node = new Node($object);
-        $node->role_system_create('r3m_io/config');
+        $package = $object->request('package');
+        if($package){
+            $node = new Node($object);
+            $node->role_system_create($package);
+        }
     }
 
     /**
@@ -24,16 +27,20 @@ trait Import {
     public function config_email(): void
     {
         $object = $this->object();
-        $options = App::options($object);
-        $class = 'System.Config.Email';
-        $options->url = $object->config('project.dir.vendor') .
-            'r3m_io/config/Data/' .
-            $class .
-            $object->config('extension.json')
-        ;
-        $node = new Node($object);
-        $response = $node->import($class, $node->role_system(), $options);
-        $this->stats($class, $response);
+        $package = $object->request('package');
+        if($package){
+            $options = App::options($object);
+            $class = 'System.Config.Email';
+            $options->url = $object->config('project.dir.vendor') .
+                $package . '/Data/' .
+                $class .
+                $object->config('extension.json')
+            ;
+            $node = new Node($object);
+            $response = $node->import($class, $node->role_system(), $options);
+            $node->stats($class, $response);
+        }
+
     }
 
     /**
@@ -42,16 +49,20 @@ trait Import {
     public function config_framework(): void
     {
         $object = $this->object();
-        $options = App::options($object);
-        $class = 'System.Config.Framework';
-        $options->url = $object->config('project.dir.vendor') .
-            'r3m_io/config/Data/' .
-            $class .
-            $object->config('extension.json')
-        ;
-        $node = new Node($object);
-        $response = $node->import($class, $node->role_system(), $options);
-        $this->stats($class, $response);
+        $package = $object->request('package');
+        if($package){
+            $options = App::options($object);
+            $class = 'System.Config.Framework';
+            $options->url = $object->config('project.dir.vendor') .
+                $package . '/Data/' .
+                $class .
+                $object->config('extension.json')
+            ;
+            $node = new Node($object);
+            $response = $node->import($class, $node->role_system(), $options);
+            $node->stats($class, $response);
+        }
+
     }
 
     /**
@@ -60,31 +71,46 @@ trait Import {
     public function config_ramdisk(): void
     {
         $object = $this->object();
-        $options = App::options($object);
-        $class = 'System.Config.Ramdisk';
-        $options->url = $object->config('project.dir.vendor') .
-            'r3m_io/config/Data/' .
-            $class .
-            $object->config('extension.json')
-        ;
-        $node = new Node($object);
-        $response = $node->import($class, $node->role_system(), $options);
-        $this->stats($class, $response);
-        $response = $node->record($class, $node->role_system(), []);
-        if(
-            $response &&
-            is_array($response) &&
-            array_key_exists('node', $response) &&
-            property_exists($response['node'], 'uuid')
-        ){
-            $uuid = Core::uuid();
-            $patch = (object) [
-                'uuid' => $response['node']->uuid,
-                'name' => $uuid,
-                'url' => $object->config('framework.dir.temp') . $uuid . $object->config('ds')
-            ];
-            $response = $node->patch($class, $node->role_system(), $patch, []);
+        $package = $object->request('package');
+        if($package){
+            $options = App::options($object);
+            $class = 'System.Config.Ramdisk';
+            $options->url = $object->config('project.dir.vendor') .
+                $package . '/Data/' .
+                $class .
+                $object->config('extension.json')
+            ;
+            $node = new Node($object);
+            $response = $node->import($class, $node->role_system(), $options);
+            $node->stats($class, $response);
+            $response = $node->record($class, $node->role_system(), []);
+            if(
+                $response &&
+                is_array($response) &&
+                array_key_exists('node', $response) &&
+                property_exists($response['node'], 'uuid')
+            ){
+                $uuid = Core::uuid();
+                $patch = (object) [
+                    'uuid' => $response['node']->uuid,
+                    'name' => $uuid,
+                    'url' => $object->config('framework.dir.temp') . $uuid . $object->config('ds')
+                ];
+                $response = $node->patch($class, $node->role_system(), $patch, []);
+                if(
+                    $response &&
+                    is_array($response) &&
+                    array_key_exists('node', $response) &&
+                    property_exists($response['node'], 'uuid') &&
+                    property_exists($response['node'], 'url') &&
+                    !empty($response['node']->uuid) &&
+                    !empty($response['node']->url)
+                ){
+                    echo 'Ramdisk patched with url ' . $response['node']->url . PHP_EOL;
+                }
+            }
         }
+
     }
 
     /**
@@ -93,16 +119,19 @@ trait Import {
     public function config_response(): void
     {
         $object = $this->object();
-        $options = App::options($object);
-        $class = 'System.Config.Response';
-        $options->url = $object->config('project.dir.vendor') .
-            'r3m_io/config/Data/' .
-            $class .
-            $object->config('extension.json')
-        ;
-        $node = new Node($object);
-        $response = $node->import($class, $node->role_system(), $options);
-        $this->stats($class, $response);
+        $package = $object->request('package');
+        if($package){
+            $options = App::options($object);
+            $class = 'System.Config.Response';
+            $options->url = $object->config('project.dir.vendor') .
+                $package . '/Data/' .
+                $class .
+                $object->config('extension.json')
+            ;
+            $node = new Node($object);
+            $response = $node->import($class, $node->role_system(), $options);
+            $node->stats($class, $response);
+        }
     }
 
     /**
@@ -111,16 +140,20 @@ trait Import {
     public function config_service(): void
     {
         $object = $this->object();
-        $options = App::options($object);
-        $class = 'System.Config.Service';
-        $options->url = $object->config('project.dir.vendor') .
-            'r3m_io/config/Data/' .
-            $class .
-            $object->config('extension.json')
-        ;
-        $node = new Node($object);
-        $response = $node->import($class, $node->role_system(), $options);
-        $this->stats($class, $response);
+        $package = $object->request('package');
+        if($package){
+            $options = App::options($object);
+            $class = 'System.Config.Service';
+            $options->url = $object->config('project.dir.vendor') .
+                $package . '/Data/' .
+                $class .
+                $object->config('extension.json')
+            ;
+            $node = new Node($object);
+            $response = $node->import($class, $node->role_system(), $options);
+            $node->stats($class, $response);
+        }
+
     }
 
     /**
@@ -129,63 +162,18 @@ trait Import {
     public function config(): void
     {
         $object = $this->object();
-        $options = App::options($object);
-        $class = 'System.Config';
-        $options->url = $object->config('project.dir.vendor') .
-            'r3m_io/config/Data/' .
-            $class .
-            $object->config('extension.json')
-        ;
-        $node = new Node($object);
-        $response = $node->import($class, $node->role_system(), $options);
-        $this->stats($class, $response);
-    }
-
-    public function stats($class, $response): void
-    {
-        if (
-            $response &&
-            array_key_exists('create', $response) &&
-            array_key_exists('put', $response) &&
-            array_key_exists('patch', $response) &&
-            array_key_exists('commit', $response) &&
-            array_key_exists('speed', $response['commit']) &&
-            array_key_exists('item_per_second', $response)
-        ) {
-            $total = $response['create'] + $response['put'] + $response['patch'];
-            if ($total === 1) {
-                echo 'Imported ' .
-                    $total .
-                    ' (create: ' .
-                    $response['create'] .
-                    ', put: ' .
-                    $response['put'] .
-                    ', patch: ' .
-                    $response['patch'] .
-                    ') item (' .
-                    $class .
-                    ') at ' .
-                    $response['item_per_second'] .
-                    ' items/sec (' .
-                    $response['commit']['speed'] . ')' .
-                    PHP_EOL;
-            } else {
-                echo 'Imported ' .
-                    $total .
-                    ' (create: ' .
-                    $response['create'] .
-                    ', put: ' .
-                    $response['put'] .
-                    ', patch: ' .
-                    $response['patch'] .
-                    ') items (' .
-                    $class .
-                    ') at ' .
-                    $response['item_per_second'] .
-                    ' items/sec (' .
-                    $response['commit']['speed'] . ')' .
-                    PHP_EOL;
-            }
+        $package = $object->request('package');
+        if($package){
+            $options = App::options($object);
+            $class = 'System.Config';
+            $options->url = $object->config('project.dir.vendor') .
+                $package . '/Data/' .
+                $class .
+                $object->config('extension.json')
+            ;
+            $node = new Node($object);
+            $response = $node->import($class, $node->role_system(), $options);
+            $node->stats($class, $response);
         }
     }
 }
