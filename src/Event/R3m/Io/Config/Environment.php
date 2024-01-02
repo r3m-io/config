65d2150ad6d2c3d2d5d48e7cd316399b0bed7e61
remco van der Velde <remco.pc@outlook.com>
2024-01-02 19:00:00 +0100

    adding events

--- src/Event/R3m/Io/Config/Environment.php
+++ src/Event/R3m/Io/Config/Environment.php
@@ -123,6 +123,7 @@ class Environment
                 }
                 if(is_array($files)){
                     foreach($files as $file){
+                        ddd($file);
                         if(
                             property_exists($file, 'chmod') &&
                             property_exists($file, 'name') &&
