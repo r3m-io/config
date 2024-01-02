5ed9bbe6583b8b831711cd5b66af9ff47bf71bd2
remco van der Velde <remco.pc@outlook.com>
2024-01-02 19:14:14 +0100

    adding events

--- src/Event/R3m/Io/Config/Environment.php
+++ src/Event/R3m/Io/Config/Environment.php
@@ -60,6 +60,10 @@ class Environment
         $config = $object->data_read($url);
         switch($environment){
             case Config::MODE_DEVELOPMENT:
+            case Config::MODE_PRODUCTION:
+            case Config::MODE_STAGING:
+            case Config::MODE_TEST:
+            case Config::MODE_REPLICA:
                 $directories = $config->data($environment . '.directory');
                 $files = $config->data($environment . '.file');
                 if(is_array($directories)){
@@ -170,120 +174,8 @@ class Environment
                 }
                 //chmod every file in application to 666 and adjust the dirs to 777
             break;
-            case Config::MODE_STAGING:
-                //chmod every file in application to File::CHMOD and adjust the dirs to Dir::CHMOD
-            break;
-            case Config::MODE_PRODUCTION:
-                $directories = $config->data($environment . '.directory');
-                $files = $config->data($environment . '.file');
-                if(is_array($directories)){
-                    foreach($directories as $directory){
-                        if(
-                            property_exists($directory, 'chmod') &&
-                            property_exists($directory->chmod, 'file') &&
-                            property_exists($directory->chmod, 'directory') &&
-                            Dir::is($object->config('project.dir.root') . $directory->name)
-                        ) {
-                            if (
-                                property_exists($directory, 'recursive') &&
-                                $directory->recursive === true
-                            ) {
-                                $command = 'chmod ' .
-                                    $directory->chmod->file . ' ' .
-                                    $object->config('project.dir.root') .
-                                    $directory->name .
-                                    ' ' .
-                                    '-R';
-                                exec($command);
-                                echo $command . PHP_EOL;
-                                $command = 'cd ' .
-                                    $object->config('project.dir.root') .
-                                    $directory->name .
-                                    $object->config('ds') .
-                                    ' && ' .
-                                    'chmod ' .
-                                    $directory->chmod->directory .
-                                    ' ' .
-                                    '.'
-                                ;;
-                                exec($command);
-                                echo $command . PHP_EOL;
-                                $dir = new Dir();
-                                $read = $dir->read(
-                                    $object->config('project.dir.root') .
-                                    $directory->name,
-                                    true
-                                );
-                                if($read){
-                                    foreach($read as $file){
-                                        if($file->type === Dir::TYPE){
-                                            $command = 'chmod ' .
-                                                $directory->chmod->directory . ' ' .
-                                                $file->url
-                                            ;
-                                            exec($command);
-                                        }
-                                    }
-                                    echo $command . PHP_EOL;
-                                }
-                                $command = 'chown ' .
-                                    $directory->owner .
-                                    ':' .
-                                    $directory->group .
-                                    ' ' .
-                                    $object->config('project.dir.root') .
-                                    $directory->name .
-                                    ' ' .
-                                    '-R';
-                                exec($command);
-                                echo $command . PHP_EOL;
-                            } else {
-                                $command = 'chmod ' .
-                                    $directory->chmod->directory . ' ' .
-                                    $object->config('project.dir.root') .
-                                    $directory->name;
-                                exec($command);
-                                echo $command . PHP_EOL;
-                                $command = 'chown ' .
-                                    $directory->owner .
-                                    ':' .
-                                    $directory->group .
-                                    ' ' .
-                                    $object->config('project.dir.root') .
-                                    $directory->name;
-                                exec($command);
-                                echo $command . PHP_EOL;
-                            }
-                        }
-                    }
-                }
-                if(is_array($files)){
-                    foreach($files as $file){
-                        if(
-                            property_exists($file, 'chmod') &&
-                            property_exists($file, 'name') &&
-                            File::exist($object->config('project.dir.root') . $file->name)
-                        ){
-                            $command = 'chmod ' .
-                                $file->chmod . ' ' .
-                                $object->config('project.dir.root') .
-                                $file->name;
-                            exec($command);
-                            echo $command . PHP_EOL;
-                            $command = 'chown ' .
-                                $file->owner .
-                                ':' .
-                                $file->group .
-                                ' ' .
-                                $object->config('project.dir.root') .
-                                $file->name;
-                            exec($command);
-                            echo $command . PHP_EOL;
-                        }
-                    }
-                }
-                //chmod every file in application to File::CHMOD and adjust the dirs to Dir::CHMOD
-            break;
+            default:
+                throw new Exception('Environment not found: ' . $environment);
         }
     }
 }
